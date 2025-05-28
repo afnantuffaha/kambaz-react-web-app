c7d53a341e1bc1f2e2c804ed7eb8f413169c870b
Afnan Tuffaha <tuffaha.af@northeastern.edu>
2025-05-27 23:23:24 -0400

    chore: add comment

--- src/Kambaz/Database/index.tsx
+++ src/Kambaz/Database/index.tsx
@@ -4,3 +4,4 @@ import assignments from "./assignments.json";
 import enrollments from "./enrollments.json";
 import users from "./users.json";
 export {  courses, modules, assignments, enrollments, users  };
+// note: chatgpt assisted with generating a lot of the data in the json files
